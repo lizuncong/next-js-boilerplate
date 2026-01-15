f541a136245bd0f91b2987d136e409b5db185c70
lizuncong <2900170896@qq.com>
2026-01-15 21:41:45 +0800

    feat: fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,26 @@
-import { request } from "@/libs/request";
+import { getImageData } from "@/services/image";
 import Image from "next/image";
+import AspectRatio from "@/components/AspectRatio";
 
 export default async function Home() {
-  const res = await request.get({ api: "https://api.thecatapi.com/v1/images2/search" }).then(
-    (res) => {
-      console.log('res..', res)
-    }
-  );
+  const res = await getImageData();
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+        {
+          res?.data?.map((item) => (
+            <AspectRatio key={item.id}>
+              <Image
+                className="dark:invert"
+                src={item.url}
+                alt="Next.js logo"
+                width={item.width}
+                height={item.height}
+                priority
+              />
+            </AspectRatio>
+          ))
+        }
       </main>
     </div>
   );
--- src/libs/request/index.ts
+++ src/libs/request/index.ts
@@ -1,10 +1,13 @@
-import { headers } from "next/headers";
-import { StatusCode } from "./response-utils";
-
-
+export enum StatusCode {
+  SUCCESS = 200,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+  INTERNAL_ERROR = 500,
+}
 
 class Request {
-  async request<Res>(options: RequestInit & { api: string }) {
+  async request<Res>(options: RequestInit & { api: string }): Promise<{msg: string; data?: Res; code: StatusCode}> {
     const { api, ...rest } = options;
 
     const config: RequestInit = {
@@ -12,44 +15,41 @@ class Request {
     };
 
     try {
-      const res = await fetch(api, config);
+      const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + api, config);
       if (!res.ok) {
-        if (res.status === 404) {
-          // not found
-        }
-        if (res.status === 400) {
-          // bad request
-        }
-        console.log('res..not ok, Bad Request', res)
         let errorMsg = '';
         try {
           errorMsg = await res.text();
         } catch {
           errorMsg = 'Can not read response text.';
         }
-
-        console.log('not ok...', errorMsg)
-        // return createErrorResponseJson(StatusCode.BAD_REQUEST);
+        console.error('request failed, status:', res.status, errorMsg)
+        if (res.status === 404) {
+          return { msg: 'Not Found', code: StatusCode.NOT_FOUND };
+        }
+        if (res.status === 400) {
+          return { msg: 'Bad Request', code: StatusCode.BAD_REQUEST };
+        }
+        return { msg: errorMsg, code: StatusCode.INTERNAL_ERROR };
       }
-      console.log('not ok...', res)
 
-      const result: Res = await res.json();
-
-      // return createResponseJson({ ...result });
+      const result = await res.json();
+      return { msg: 'Success', data: result, code: StatusCode.SUCCESS };
     } catch (err) {
+      console.error('unknown error, error:', err)
       if (err.cause) {
         const { code, errno, syscall, hostname } = err.cause
         if (code === 'ENOTFOUND') {
           console.error('DNS 解析失败，域名：', hostname);
-          return
+          return { msg: 'DNS not found', code: StatusCode.NOT_FOUND };
         }
       }
 
-      // return createErrorResponseJson(StatusCode.INTERNAL_ERROR);
+      return { msg: 'Internal error', code: StatusCode.INTERNAL_ERROR };
     }
   }
 
-  get<Req, Res>(options: { api: string; params?: Req; headers?: HeadersInit }) {
+  get<Res>(options: { api: string; params?: URLSearchParams; headers?: HeadersInit }) {
     const { api, params, headers } = options;
     return this.request<Res>({
       api: params ? api + `?${new URLSearchParams(params).toString()}` : api,
@@ -58,7 +58,7 @@ class Request {
     });
   }
 
-  post<Req, Res>(options: { api: string; params: Req; headers?: HeadersInit }) {
+  post<Res>(options: { api: string; params: unknown; headers?: HeadersInit }) {
     const { api, params, headers } = options;
     return this.request<Res>({
       api: api,
